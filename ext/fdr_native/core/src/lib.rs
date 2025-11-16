fdea4352a3157784a1c775daa8d3d2772279a25f
Shannon Skipper <shannonskipper@gmail.com>
2025-11-16 14:11:16 -0800

    Remove jemalloc due to issues on macOS & Linux
    Jemalloc is broken on macOS and Linux at the moment, which are our OSes
    for CI. Since it's uncomfortably buggy, removing it for now despite a
    nice performance improvement. Maybe we can consider mimalloc.

--- ext/fdr_native/core/src/lib.rs
+++ ext/fdr_native/core/src/lib.rs
@@ -1,17 +1,4 @@
 //! File search library in the style of `fd`
-#[cfg(all(
-    not(windows),
-    not(target_os = "android"),
-    not(target_os = "macos"),
-    not(target_os = "freebsd"),
-    not(target_os = "openbsd"),
-    not(target_os = "illumos"),
-    not(all(target_env = "musl", target_pointer_width = "32")),
-    not(target_arch = "riscv64")
-))]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
-
 use anyhow::Result;
 use std::path::PathBuf;
 
