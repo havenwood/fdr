2b8e9c7e3bf24bdaab1b00c1a97a9b385e833690
Shannon Skipper <shannonskipper@gmail.com>
2025-11-16 14:34:38 -0800

    Switch from **options to actual kwargs

--- ext/fdr_native/ffi/src/lib.rs
+++ ext/fdr_native/ffi/src/lib.rs
@@ -2,130 +2,126 @@
 #![allow(unsafe_code, reason = "FFI requires unsafe for Ruby interop")]
 
 use fdr_core::{SearchConfig, search};
-use magnus::{Error, RArray, RHash, Ruby, TryConvert, function, prelude::*};
+use magnus::scan_args::scan_args;
+use magnus::{Error, RArray, RHash, Ruby, TryConvert, Value, function, prelude::*};
 use std::path::PathBuf;
 
-fn extract_bool(ruby: &Ruby, options: RHash, key: &str) -> Result<Option<bool>, Error> {
-    if let Some(val) = options.get(ruby.to_symbol(key))
-        && !val.is_nil()
-    {
-        Ok(Some(TryConvert::try_convert(val)?))
-    } else {
-        Ok(None)
-    }
-}
-
-fn extract_depth(ruby: &Ruby, options: RHash, key: &str) -> Result<Option<usize>, Error> {
-    if let Some(val) = options.get(ruby.to_symbol(key))
-        && !val.is_nil()
-    {
-        let depth: i64 = TryConvert::try_convert(val)?;
-        let depth_usize = usize::try_from(depth).map_err(|_| {
-            Error::new(
-                ruby.exception_arg_error(),
-                format!("{key} must be a non-negative integer, got {depth}"),
-            )
-        })?;
-        Ok(Some(depth_usize))
-    } else {
-        Ok(None)
-    }
-}
-
-fn extract_size(ruby: &Ruby, options: RHash, key: &str) -> Result<Option<u64>, Error> {
-    if let Some(val) = options.get(ruby.to_symbol(key))
-        && !val.is_nil()
-    {
-        let size: i64 = TryConvert::try_convert(val)?;
-        let size_u64 = u64::try_from(size).map_err(|_| {
-            Error::new(
-                ruby.exception_arg_error(),
-                format!("{key} must be a non-negative integer, got {size}"),
-            )
-        })?;
-        Ok(Some(size_u64))
-    } else {
-        Ok(None)
-    }
-}
-
-fn extract_time(ruby: &Ruby, options: RHash, key: &str) -> Result<Option<i64>, Error> {
-    if let Some(val) = options.get(ruby.to_symbol(key))
-        && !val.is_nil()
-    {
-        let seconds: i64 = TryConvert::try_convert(val)?;
-        if seconds < 0 {
-            return Err(Error::new(
-                ruby.exception_arg_error(),
-                format!("{key} must be a non-negative integer, got {seconds}"),
-            ));
+fn extract_optional_arg<T: TryConvert>(ruby: &Ruby, hash: RHash, key: &str) -> Option<T> {
+    hash.get(ruby.to_symbol(key)).and_then(|val| {
+        if val.is_nil() {
+            None
+        } else {
+            TryConvert::try_convert(val).ok()
         }
-        Ok(Some(seconds))
-    } else {
-        Ok(None)
+    })
+}
+
+struct SearchParams {
+    pattern: Option<String>,
+    paths: Option<RArray>,
+    hidden: Option<bool>,
+    no_ignore: Option<bool>,
+    case_sensitive: Option<bool>,
+    glob: Option<bool>,
+    full_path: Option<bool>,
+    follow: Option<bool>,
+    max_depth: Option<i64>,
+    min_depth: Option<i64>,
+    file_type: Option<String>,
+    extension: Option<String>,
+    exclude: Option<RArray>,
+    min_size: Option<i64>,
+    max_size: Option<i64>,
+    changed_within: Option<i64>,
+    changed_before: Option<i64>,
+}
+
+fn extract_search_params(ruby: &Ruby, kwargs: RHash) -> SearchParams {
+    SearchParams {
+        pattern: extract_optional_arg(ruby, kwargs, "pattern"),
+        paths: extract_optional_arg(ruby, kwargs, "paths"),
+        hidden: extract_optional_arg(ruby, kwargs, "hidden"),
+        no_ignore: extract_optional_arg(ruby, kwargs, "no_ignore"),
+        case_sensitive: extract_optional_arg(ruby, kwargs, "case_sensitive"),
+        glob: extract_optional_arg(ruby, kwargs, "glob"),
+        full_path: extract_optional_arg(ruby, kwargs, "full_path"),
+        follow: extract_optional_arg(ruby, kwargs, "follow"),
+        max_depth: extract_optional_arg(ruby, kwargs, "max_depth"),
+        min_depth: extract_optional_arg(ruby, kwargs, "min_depth"),
+        file_type: extract_optional_arg(ruby, kwargs, "type"),
+        extension: extract_optional_arg(ruby, kwargs, "extension"),
+        exclude: extract_optional_arg(ruby, kwargs, "exclude"),
+        min_size: extract_optional_arg(ruby, kwargs, "min_size"),
+        max_size: extract_optional_arg(ruby, kwargs, "max_size"),
+        changed_within: extract_optional_arg(ruby, kwargs, "changed_within"),
+        changed_before: extract_optional_arg(ruby, kwargs, "changed_before"),
     }
 }
 
-fn hash_to_config(ruby: &Ruby, options: RHash) -> Result<SearchConfig, Error> {
+fn build_search_config(ruby: &Ruby, params: SearchParams) -> Result<SearchConfig, Error> {
     let mut config = SearchConfig::default();
 
-    if let Some(val) = options.get(ruby.to_symbol("pattern"))
-        && !val.is_nil()
-    {
-        let pattern: String = TryConvert::try_convert(val)?;
+    if let Some(pattern) = params.pattern {
         config.pattern = Some(pattern);
     }
 
-    if let Some(val) = options.get(ruby.to_symbol("paths"))
-        && !val.is_nil()
-    {
-        let paths_array: RArray = TryConvert::try_convert(val)?;
-        let mut paths = Vec::with_capacity(paths_array.len());
+    if let Some(paths_array) = params.paths {
+        let mut paths_vec = Vec::with_capacity(paths_array.len());
         for path_val in paths_array {
             let path_str: String = TryConvert::try_convert(path_val)?;
-            paths.push(PathBuf::from(path_str));
+            paths_vec.push(PathBuf::from(path_str));
         }
-        config.paths = paths;
+        config.paths = paths_vec;
     }
 
-    if let Some(val) = extract_bool(ruby, options, "hidden")? {
-        config.hidden = val;
+    if let Some(hidden) = params.hidden {
+        config.hidden = hidden;
     }
-    if let Some(val) = extract_bool(ruby, options, "no_ignore")? {
-        config.no_ignore = val;
+    if let Some(no_ignore) = params.no_ignore {
+        config.no_ignore = no_ignore;
     }
-    if let Some(val) = extract_bool(ruby, options, "case_sensitive")? {
-        config.case_sensitive = val;
+    if let Some(case_sensitive) = params.case_sensitive {
+        config.case_sensitive = case_sensitive;
     }
-    if let Some(val) = extract_bool(ruby, options, "glob")? {
-        config.glob = val;
+    if let Some(glob) = params.glob {
+        config.glob = glob;
     }
-    if let Some(val) = extract_bool(ruby, options, "full_path")? {
-        config.full_path = val;
+    if let Some(full_path) = params.full_path {
+        config.full_path = full_path;
     }
-    if let Some(val) = extract_bool(ruby, options, "follow")? {
-        config.follow = val;
-    }
-
-    config.max_depth = extract_depth(ruby, options, "max_depth")?;
-    config.min_depth = extract_depth(ruby, options, "min_depth")?;
-
-    if let Some(val) = options.get(ruby.to_symbol("type"))
-        && !val.is_nil()
-    {
-        config.file_type = Some(TryConvert::try_convert(val)?);
+    if let Some(follow) = params.follow {
+        config.follow = follow;
     }
 
-    if let Some(val) = options.get(ruby.to_symbol("extension"))
-        && !val.is_nil()
-    {
-        config.extension = Some(TryConvert::try_convert(val)?);
+    if let Some(max_depth) = params.max_depth {
+        let max_depth_usize = usize::try_from(max_depth).map_err(|_| {
+            Error::new(
+                ruby.exception_arg_error(),
+                format!("max_depth must be a non-negative integer, got {max_depth}"),
+            )
+        })?;
+        config.max_depth = Some(max_depth_usize);
     }
 
-    if let Some(val) = options.get(ruby.to_symbol("exclude"))
-        && !val.is_nil()
-    {
-        let exclude_array: RArray = TryConvert::try_convert(val)?;
+    if let Some(min_depth) = params.min_depth {
+        let min_depth_usize = usize::try_from(min_depth).map_err(|_| {
+            Error::new(
+                ruby.exception_arg_error(),
+                format!("min_depth must be a non-negative integer, got {min_depth}"),
+            )
+        })?;
+        config.min_depth = Some(min_depth_usize);
+    }
+
+    if let Some(file_type) = params.file_type {
+        config.file_type = Some(file_type);
+    }
+
+    if let Some(extension) = params.extension {
+        config.extension = Some(extension);
+    }
+
+    if let Some(exclude_array) = params.exclude {
         let mut excludes = Vec::with_capacity(exclude_array.len());
         for exclude_val in exclude_array {
             excludes.push(TryConvert::try_convert(exclude_val)?);
@@ -133,17 +129,53 @@ fn hash_to_config(ruby: &Ruby, options: RHash) -> Result<SearchConfig, Error> {
         config.exclude = excludes;
     }
 
-    config.min_size = extract_size(ruby, options, "min_size")?;
-    config.max_size = extract_size(ruby, options, "max_size")?;
+    if let Some(min_size) = params.min_size {
+        let min_size_u64 = u64::try_from(min_size).map_err(|_| {
+            Error::new(
+                ruby.exception_arg_error(),
+                format!("min_size must be a non-negative integer, got {min_size}"),
+            )
+        })?;
+        config.min_size = Some(min_size_u64);
+    }
 
-    config.changed_within = extract_time(ruby, options, "changed_within")?;
-    config.changed_before = extract_time(ruby, options, "changed_before")?;
+    if let Some(max_size) = params.max_size {
+        let max_size_u64 = u64::try_from(max_size).map_err(|_| {
+            Error::new(
+                ruby.exception_arg_error(),
+                format!("max_size must be a non-negative integer, got {max_size}"),
+            )
+        })?;
+        config.max_size = Some(max_size_u64);
+    }
+
+    if let Some(changed_within) = params.changed_within {
+        if changed_within < 0 {
+            return Err(Error::new(
+                ruby.exception_arg_error(),
+                format!("changed_within must be a non-negative integer, got {changed_within}"),
+            ));
+        }
+        config.changed_within = Some(changed_within);
+    }
+
+    if let Some(changed_before) = params.changed_before {
+        if changed_before < 0 {
+            return Err(Error::new(
+                ruby.exception_arg_error(),
+                format!("changed_before must be a non-negative integer, got {changed_before}"),
+            ));
+        }
+        config.changed_before = Some(changed_before);
+    }
 
     Ok(config)
 }
 
-fn fdr_search(ruby: &Ruby, options: RHash) -> Result<RArray, Error> {
-    let config = hash_to_config(ruby, options)?;
+fn fdr_search(ruby: &Ruby, args: &[Value]) -> Result<RArray, Error> {
+    let args_scan = scan_args::<(), (), (), (), RHash, ()>(args)?;
+    let params = extract_search_params(ruby, args_scan.keywords);
+    let config = build_search_config(ruby, params)?;
 
     if let (Some(min), Some(max)) = (config.min_depth, config.max_depth)
         && min > max
@@ -169,9 +201,9 @@ fn fdr_search(ruby: &Ruby, options: RHash) -> Result<RArray, Error> {
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let fdr_module = ruby.define_module("Fdr")?;
 
-    fdr_module.define_singleton_method("search", function!(fdr_search, 1))?;
-    fdr_module.define_singleton_method("entries", function!(fdr_search, 1))?;
-    fdr_module.define_singleton_method("scan", function!(fdr_search, 1))?;
+    fdr_module.define_singleton_method("search", function!(fdr_search, -1))?;
+    fdr_module.define_singleton_method("entries", function!(fdr_search, -1))?;
+    fdr_module.define_singleton_method("scan", function!(fdr_search, -1))?;
 
     Ok(())
 }
