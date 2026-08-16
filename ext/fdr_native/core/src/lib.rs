490e07d5fa473472f22c6a866a81cd52fd28e1e1
Shannon Skipper <shannonskipper@gmail.com>
2026-08-16 10:54:15 -0700

    Validate `type` values, strip `extension` dots

--- ext/fdr_native/core/src/lib.rs
+++ ext/fdr_native/core/src/lib.rs
@@ -108,6 +108,7 @@ fn build_extension_regex(
     use regex::bytes::RegexBuilder;
 
     if let Some(ref ext) = config.extension {
+        let ext = ext.strip_prefix('.').unwrap_or(ext);
         let pattern = format!(r"\.{}$", regex::escape(ext));
         Ok(Some(
             RegexBuilder::new(&pattern)
