1b9c05dfeeb94c12f3a51aec113d34adeadaf6c7
Shannon Skipper <shannonskipper@gmail.com>
2026-08-16 08:40:17 -0700

    Add specs pinning kwarg and loader edge cases

--- spec/native_loader_spec.rb
+++ spec/native_loader_spec.rb
@@ -18,6 +18,16 @@ class NativeLoaderSpec < Minitest::Test
     end
   end
 
+  def test_reports_a_missing_native_extension
+    Dir.mktmpdir("fdr-native-loader") do |directory|
+      lib = build_library(directory, nil)
+      stderr, status = load_fdr(lib)
+
+      refute status.success?, "loading without any native extension should fail"
+      assert_match(%r{fdr/fdr_native}, stderr)
+    end
+  end
+
   private
 
   def compiled_extension
@@ -30,7 +40,7 @@ class NativeLoaderSpec < Minitest::Test
     FileUtils.mkdir_p(fdr)
     FileUtils.cp(File.expand_path("../lib/fdr.rb", __dir__), lib)
     FileUtils.cp(File.expand_path("../lib/fdr/version.rb", __dir__), fdr)
-    FileUtils.cp(extension, File.join(fdr, File.basename(extension)))
+    FileUtils.cp(extension, File.join(fdr, File.basename(extension))) if extension
     lib
   end
 
