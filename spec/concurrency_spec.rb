e03e2457533c19d00ff898aaeddafe338a41cc6b
Shannon Skipper <shannonskipper@gmail.com>
2026-08-20 06:12:21 -0700

    Release GVL during search and grep

---
+++ spec/concurrency_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+require 'fileutils'
+require 'tmpdir'
+
+describe 'Fdr concurrency' do
+  before do
+    @dir = Dir.mktmpdir('fdr-concurrency')
+    20.times do |i|
+      subdir = File.join(@dir, "dir#{i}")
+      Dir.mkdir(subdir)
+      100.times { |j| File.write(File.join(subdir, "file#{j}.txt"), "needle\n") }
+    end
+  end
+
+  after do
+    FileUtils.remove_entry(@dir)
+  end
+
+  def ticks_during
+    ticks = 0
+    thread = Thread.new { loop { ticks += 1 } }
+    sleep 0.01 while ticks.zero?
+    before = ticks
+    yield
+    during = ticks - before
+    thread.kill
+    thread.join
+    during
+  end
+
+  it 'releases the GVL so other threads run during search' do
+    during = ticks_during { Fdr.search(paths: [@dir], hidden: true) }
+    assert_predicate during, :positive?, 'other threads should run during Fdr.search'
+  end
+
+  it 'releases the GVL so other threads run during grep' do
+    during = ticks_during { Fdr.grep(pattern: 'needle', paths: [@dir]) }
+    assert_predicate during, :positive?, 'other threads should run during Fdr.grep'
+  end
+end
